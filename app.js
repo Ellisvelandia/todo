73d6a45b96538579354a276cc0e161974bf473c4
Ellisvelandia <eyis619l@gmail.com>
2023-05-01 22:09:23 -0500

    todo

--- app.js
+++ app.js
@@ -2,15 +2,27 @@ const todoInput = document.getElementById("todoInput");
 const addBtn = document.getElementById("addBtn");
 const todoList = document.getElementById("todoList");
 
-addBtn.addEventListener("click", addTodo)
+addBtn.addEventListener("click", addTodo);
 
 function addTodo() {
   const todoText = todoInput.value;
 
   const listItem = document.createElement("li");
+  const deleteItem = document.createElement("p");
+  deleteItem.classList.add("delete");
+  deleteItem.innerText = "x";
   listItem.innerText = todoText;
+ 
 
   todoList.appendChild(listItem);
-
+  listItem.appendChild(deleteItem);
   todoInput.value = "";
+
+  deleteItem.addEventListener("click", deleteTodo);
+}
+
+function deleteTodo() {
+  const listItem = this.parentNode;
+  const todoList = listItem.parentNode;
+  todoList.removeChild(listItem);
 }
